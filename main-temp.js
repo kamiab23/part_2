3d9aed94384dfb59665c22f47ea3349b5fc0ffdd
ali-parhizain <kamiabparhizain23@gmail.com>
2024-02-10 19:50:11 +0330

    this version is with for

--- main-temp.js
+++ main-temp.js
@@ -1,18 +1,27 @@
-function findMax() {}
+var inpt = document.querySelector("#inpt");
+var h2 = document.querySelector("h2");
+var p = document.querySelector("p");
+var show = 0;
+function findMax() {
+  var arr = inpt.value.split(" ");
+  for (let i = 0; i < arr.length; i++) {
+    if (+arr[i] > +show) {
+      console.log(arr[i], show);
+      show = arr[i];
+    }
+    console.log(arr[i], show);
+  }
+  h2.innerHTML = "Max: " + show;
+}
 
-function findMin() {}
+function findMin() {
+  var arr = inpt.value.split(" ");
+}
 
 function removeDuplicate() {
-  
-    //method 1
-   
-
-    //method 2
-    
-
-    //method 3 
-    
-
+  //method 1
+  //method 2
+  //method 3
 }
 
 function findDuplicates() {}
