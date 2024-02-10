6554aff6ac10b4c666b6b8f43f0fd9a939d96f4b
ali-parhizain <kamiabparhizain23@gmail.com>
2024-02-10 17:51:37 +0330

    first step

---
+++ main-temp.js
@@ -0,0 +1,18 @@
+function findMax() {}
+
+function findMin() {}
+
+function removeDuplicate() {
+  
+    //method 1
+   
+
+    //method 2
+    
+
+    //method 3 
+    
+
+}
+
+function findDuplicates() {}
