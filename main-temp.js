5b522ea3301dde3d91c46f0fc25ed8c219595d53
ali-parhizain <kamiabparhizain23@gmail.com>
2024-02-12 17:21:15 +0330

    final version

--- main-temp.js
+++ main-temp.js
@@ -1,34 +1,43 @@
 var inpt = document.querySelector("#inpt");
 var result = document.querySelector(".result");
 var h2 = document.querySelector("h2");
-var p = document.querySelector("p");
+// var p = document.querySelector("p");
 var show = 0;
 function findMax() {
   var arr = inpt.value.split(" ");
-    arr.forEach(element => {
-        console.log(element);
-        if (+element > +show) {
-            show = element;
-        }
-    });
-    h2.innerHTML = "Max: " + show;
+  arr.forEach((element) => {
+    // console.log(element);
+    if (+element > +show) {
+      show = element;
+    }
+  });
+  h2.innerHTML = "Max: " + show;
 }
 
 function findMin() {
   var arr = inpt.value.split(" ");
-    show = arr[0];
-    arr.forEach(element => {
-        if (+element < +show) {
-            show = element;
-        }
-    })
-    h2.innerHTML = "Min: " + show;
+  show = arr[0];
+  arr.forEach((element) => {
+    if (+element < +show) {
+      show = element;
+    }
+  });
+  h2.innerHTML = "Min: " + show;
 }
 
 function removeDuplicate() {
-  //method 1
-  //method 2
-  //method 3
+    var arr = inpt.value.split(" ");
+    var uniqueArr = arr.filter((value, index, self) => {
+      return self.indexOf(value) === index;
+    });
+    inpt.value = uniqueArr.join(" ");
+    h2.innerHTML = "RemoveDuplicate: " + uniqueArr;
 }
 
-function findDuplicates() {}
+function findDuplicates() {
+  var arr = inpt.value.split(" ");
+  var duplicates = arr.filter((value, index, self) => {
+    return self.indexOf(value) !== index;
+  });
+  h2.innerHTML = "Duplicates " + duplicates;
+}
